d847e4f84ae637dbcb488651d906430a2ae4b791
Diego Santos <63413446+Diegodevops26@users.noreply.github.com>
2023-04-08 19:40:43 -0300

    Add files via upload

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import style from './Button.module.scss';
+
+interface Props {
+  type?: "button" | "submit" | "reset" | undefined,
+  onClick?: () => void,
+  children?: React.ReactNode
+}
+
+function Button({ onClick, type, children }: Props) {
+  return (
+    <button
+      onClick={onClick}
+      type={type}
+      className={style.botao}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button;
